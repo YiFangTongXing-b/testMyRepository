8773ee9bff3b3d5f2802080dde3e627711ef2aff
YiFangTongXing-b <85434996+YiFangTongXing-b@users.noreply.github.com>
2022-09-04 15:44:00 +0800

    Delete computerShop directory

--- computerShop/src/main/java/com/czf/computer/mapper/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package com.czf.computer.mapper;
-
-import com.czf.computer.domain.Order;
-import com.czf.computer.domain.OrderItem;
-
-public interface OrderMapper {
-    int insertOrder(Order order);                   //插入订单
-    int insertOrderItem(OrderItem orderItem);       //插入订单项
-}
--- computerShop/src/main/java/com/czf/computer/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.czf.computer.service;
-
-
-import com.czf.computer.domain.Order;
-
-
-public interface OrderService {
-    Order create(int aid, int[] cids, int uid, String username);
-}
